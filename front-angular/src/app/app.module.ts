a7a2ea79286baca71ef38654dc3b2b80c9195811
n1c0145 <nicolas.lozalml@gmail.com>
2022-07-25 10:34:36 -0500

    Adding and validating reactive forms

--- front-angular/src/app/app.module.ts
+++ front-angular/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
 import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
 
@@ -14,7 +15,8 @@ import { ListarProductosComponent } from './components/listar-productos/listar-p
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

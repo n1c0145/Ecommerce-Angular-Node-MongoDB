1f6f52cdac6c0b05ded57fa4a8ca781322bbe93f
n1c0145 <nicolas.lozalml@gmail.com>
2022-07-25 09:02:28 -0500

    Configure fail route redirect

--- front-angular/src/app/app-routing.module.ts
+++ front-angular/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { ListarProductosComponent } from './components/listar-productos/listar-p
 
 const routes: Routes = [{ path: '', component: ListarProductosComponent },
 {path: 'crear-producto',component:CrearProductoComponent},
-{path: 'editar-producto/:id',component:CrearProductoComponent}
+{path: 'editar-producto/:id',component:CrearProductoComponent},
+{path: '**',redirectTo:'',pathMatch:'full'}
 ];
 
 @NgModule({

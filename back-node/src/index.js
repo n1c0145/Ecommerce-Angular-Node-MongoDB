049ab5425ef7aa030831b352a30e25931d636e1f
n1c0145 <nicolas.lozalml@gmail.com>
2022-07-25 16:32:54 -0500

    Configuring middelwares

--- back-node/src/index.js
+++ back-node/src/index.js
@@ -10,6 +10,13 @@ db();
 // Settings
 app.set("port", process.env.PORT || 3000);
 
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Routes
+app.use('/api/productos', require('./routes/producto'));
+
 // Starting the server
 app.listen(app.get("port"), () => {
     console.log(`Server on port ${app.get("port")}`);
---
+++ back-node/src/routes/producto.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+//Crear producto
+router.post("/", ()=>{
+    console.log(creando);
+})
+
+module.exports = router;

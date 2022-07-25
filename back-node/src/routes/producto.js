68317d051a262e8b42faeaca30f32662e9680c15
n1c0145 <nicolas.lozalml@gmail.com>
2022-07-25 16:42:42 -0500

    Route method crear

--- back-node/src/routes/producto.js
+++ back-node/src/routes/producto.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 //Crear producto
-router.post("/", ()=>{
-    console.log(creando);
-})
+router.post("/", async (req, res) => {
+  try {
+    let producto = new Producto(req.body);
+    await producto.save();
+    res.send(producto);
+  } catch (error) {
+    console.log(error);
+    resizeBy.status(500).send("Error");
+  }
+});
 
 module.exports = router;

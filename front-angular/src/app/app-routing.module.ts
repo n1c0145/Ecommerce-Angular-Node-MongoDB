f4f9db51549fda1cd0a097c374522ba7abb898e6
n1c0145 <nicolas.lozalml@gmail.com>
2022-07-25 09:53:41 -0500

    Creating styles components listarproducto and crearproducto

--- front-angular/src/app/app-routing.module.ts
+++ front-angular/src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
 import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
 
-const routes: Routes = [{ path: '', component: ListarProductosComponent },
-{path: 'crear-producto',component:CrearProductoComponent},
-{path: 'editar-producto/:id',component:CrearProductoComponent},
-{path: '**',redirectTo:'',pathMatch:'full'}
+const routes: Routes = [
+  { path: '', component: ListarProductosComponent },
+  { path: 'crear-producto', component: CrearProductoComponent },
+  { path: 'editar-producto/:id', component: CrearProductoComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
 @NgModule({

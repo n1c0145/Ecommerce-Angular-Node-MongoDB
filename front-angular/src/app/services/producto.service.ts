8b37318c81f072e5ae1cf118d212d3f0e90e55b4
n1c0145 <nicolas.lozalml@gmail.com>
2022-07-26 15:56:54 -0500

    Fix port

--- front-angular/src/app/services/producto.service.ts
+++ front-angular/src/app/services/producto.service.ts
@@ -7,7 +7,7 @@ import { Producto } from "../models/producto";
   providedIn: 'root'
 })
 export class ProductoService {
-  url = 'http://localhost:4000/api/productos/';
+  url = 'http://localhost:3000/api/productos/';
 
   constructor(private http: HttpClient) { }
 

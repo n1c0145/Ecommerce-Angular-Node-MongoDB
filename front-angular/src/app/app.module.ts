9b1e95a6fb0c8ce5610b7945ffaac4e5f1ebf7f4
n1c0145 <nicolas.lozalml@gmail.com>
2022-07-28 14:53:33 -0500

    Login template and register method

--- front-angular/src/app/app.module.ts
+++ front-angular/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { HttpClientModule } from "@angular/common/http";
@@ -31,7 +31,8 @@ import { NavComponent } from './nav/nav.component';
     ReactiveFormsModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-angular/src/app/registro/registro.component.ts
+++ front-angular/src/app/registro/registro.component.ts
@@ -1,3 +1,6 @@
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {
   }
 
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: '',
+      email: '',
+      password: ''
+    });
+  }
+
+  submit(): void {
+    this.http.post('http://localhost:3000/api/auth/register', this.form.getRawValue())
+      .subscribe(() => this.router.navigate(['/login']));
+  }
 }

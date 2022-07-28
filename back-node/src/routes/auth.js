8dc676e2be858a1a0913eeac3d55a49a47525fcc
n1c0145 <nicolas.lozalml@gmail.com>
2022-07-28 09:15:57 -0500

    Login route

--- back-node/src/routes/auth.js
+++ back-node/src/routes/auth.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const User = require("../models/Auth");
+const jwt = require('jsonwebtoken')
 
+//Create user
 router.post("/register", async (req, res) => {
   try {
     const salt = await bcrypt.genSalt(10);
@@ -19,6 +21,36 @@ router.post("/register", async (req, res) => {
     console.log(error);
     res.status(500).send("Error");
   }
+});
+
+//Login user
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({email: req.body.email})
+        if (!user) {
+            return res.status(404).send({
+                message: 'user not found'
+            })
+        }
+        if (!await bcrypt.compare(req.body.password, user.password)) {
+            return res.status(400).send({
+                message: 'invalid credentials'
+            })
+        }
+        const token = jwt.sign({_id: user._id}, "secret")
+        res.cookie('jwt', token, {
+            httpOnly: true,
+            maxAge: 24 * 60 * 60 * 1000 // 1 day
+        })
+        res.send({
+            mesage:'success'
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error");
+    }
 });
 
 module.exports = router;

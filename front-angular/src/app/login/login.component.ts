ae6d3c23af20edae953d24712e3fbd4f9237fcba
n1c0145 <nicolas.lozalml@gmail.com>
2022-07-28 15:31:46 -0500

    Login method

--- front-angular/src/app/login/login.component.ts
+++ front-angular/src/app/login/login.component.ts
@@ -1,3 +1,6 @@
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {
   }
 
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: '',
+      password: ''
+    });
+  }
+
+  submit(): void {
+    this.http.post('http://localhost:3000/api/auth/login', this.form.getRawValue(), {
+      withCredentials: true
+    }).subscribe(() => this.router.navigate(['/listar-producto']));
+  }
 }
